0291a4f60f0e898863beeecade83d21eb0e6b709
Beiri Alife <beiri.al@outlook.com>
2024-08-10 16:14:15 -0300

    2.12 - Continue

--- adivinhacao.c
+++ adivinhacao.c
@@ -19,6 +19,14 @@ int main()
         scanf("%d", &chute);
         printf("Seu chute foi %d\n", chute);
 
+        if (chute < 0)
+        {
+            printf("Você não pode chutar números negativos!\n");
+            i--;
+
+            continue;
+        }
+
         int acertou = (chute == numerosecreto);
         int maior = chute > numerosecreto;
 
