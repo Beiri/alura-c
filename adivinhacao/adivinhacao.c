24b6d1d8cbdb76156d39018c36b7728b281309b8
Beiri Alife <beiri.al@outlook.com>
2024-08-10 02:25:26 -0300

    1.6 - Lidando com erros

---
+++ adivinhacao/adivinhacao.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Imprime o cabeçalho do jogo.
+    printf("******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+    printf("******************************************\n");
+
+    return 0;
+}
