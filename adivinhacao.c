f08b6fa961455d69cbb7947c0cb8843fb1577f4a
Beiri Alife <beiri.al@outlook.com>
2024-08-11 15:51:41 -0300

    3.8 - Convertendo tipos de variáveis

--- adivinhacao.c
+++ adivinhacao.c
@@ -44,7 +44,7 @@ int main()
 
         tentativas++;
 
-        double pontosperdidos = (chute - numerosecreto) / 2.0;
+        double pontosperdidos = (chute - numerosecreto) / (double)2;
         pontos = pontos - pontosperdidos;
     }
 
