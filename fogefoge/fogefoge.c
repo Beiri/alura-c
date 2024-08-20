1402183e4d268dfe15d5d870d5fde5e174f611cb
Beiri Alife <beiri.al@outlook.com>
2024-08-20 19:28:19 -0300

    1.04 - Salvando e lendo dados de uma matriz

--- fogefoge/fogefoge.c
+++ fogefoge/fogefoge.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    char mapa[5][10];
+    char mapa[5][10 + 1];
 
-    mapa[0][0] = '|';
-    mapa[4][9] = '@';
+    FILE *f;
+    f = fopen("mapa.txt", "r");
 
-    printf("%c %c\n", mapa[0][0], mapa[4][9]);
+    if (f == 0)
+    {
+        printf("Erro na leitura do mapa\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        fscanf(f, "%s", mapa[i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", mapa[i]);
+    }
+
+    fclose(f);
 }
